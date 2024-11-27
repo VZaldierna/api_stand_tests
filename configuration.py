10ab54589e060004bf67375fc150d8bd739377ce
Vanessa <nessa@Jorges-MacBook-Pro.local>
2024-11-26 22:03:53 -0800

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://cnt-be7127ce-0816-4ab1-8b23-09dc55c18d17.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
